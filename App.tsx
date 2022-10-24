cd077322de9bec832c8d4a7cdee530cbe74db7ca
LEONARDO LISBOA <leonardo.lisboa@grupofasitec.com.br>
2022-10-24 12:23:33 -0300

    Configuração para publicar o app na play store

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Text, StatusBar} from 'react-native'
 import Home from './src/pages/Home'
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
+
+  useEffect(() => {
+    SplashScreen.hide()//isso é para fazer com que a splash screen sai até q tudo seja carregado, por isso coloquei no useeffect, pois o effect esperar carregar tudo para ser chamado
+  }, [])
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
